55a9dc1a08addbfc013483368546b16b1f65534d
FXXDEV <fxxdev@outlook.com>
2018-05-02 19:50:50 -0300

    Upload teste encadeada

---
+++ Lista Encadeada/Encadeada-teste(Ordenação).cpp
@@ -0,0 +1,292 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int exec=0;
+int verificador=0;
+struct lista
+{
+	int valor;
+	struct lista *aa;
+	struct lista *pp;
+};
+
+struct lista *inicia(){
+	return NULL;
+}
+
+int main();
+
+struct lista *finalera;
+
+struct lista *buscar(struct lista *list, int val) {
+	struct lista *voltar; 
+	struct lista *primeiro;
+	struct lista *ultimo;
+	int cont=0;
+	int w=0;
+	int encontrado = 0; 
+	voltar = NULL; 
+	
+
+
+	do{
+		if((list->valor) != val) {
+
+			list = list->pp; 
+			cont++;
+		
+			if(cont==1){
+				primeiro = list->aa;
+			}else if(list->pp == NULL){
+				list->pp = primeiro;
+			}
+			
+		}else if((list->valor) == val){ 
+		
+			voltar = list; 
+			encontrado = 1;
+			break;
+		}
+		
+		w++;
+		
+	}while(w < verificador+1);
+
+
+	if(encontrado == 1 && verificador ==1){
+		int *imprime;
+		
+		imprime= &voltar->valor;
+		printf("Valor encontrado!\n");
+		printf("\nValor anterior:%i   |   ",*imprime);
+		printf("Valor:%i   |   ",*imprime);
+		printf("Valor próximo:%i\n",*imprime);
+	}else if(encontrado == 1){
+			 
+		printf("Valor encontrado!\n");
+			
+		int *imprime;
+		int *anterior,*antF;
+		int *proximo,*proxF;
+		struct lista *prox;
+		struct lista *ant;
+		prox = voltar;
+		ant = voltar;
+	
+		imprime= &voltar->valor;
+		proximo= &prox->pp->valor;
+		anterior= &ant->aa->valor;
+		
+		printf("\nValor anterior:%i   |   ",*anterior);
+		printf("Valor:%i   |   ",*imprime);
+		printf("Valor próximo:%i\n",*proximo);
+	}else if(encontrado !=1){
+		printf("Valor inválido\n");
+		
+	}
+	
+	return voltar;
+	main();
+
+}
+
+void  *listar(struct lista *list){
+	if(verificador==NULL){
+		printf("Lista vazia, insira alguns valores\n");
+		system("pause");
+		main();
+	}else{
+		printf("Lista ordenada\n");
+		struct lista *aux =list;
+	
+		for(int y=0;y<verificador;y++){
+			printf("Valor:%i\n",aux->valor);
+			aux = aux->pp;
+		}
+		
+		
+	}
+	
+
+}
+
+struct lista *insereLista(struct lista *list,int val){
+	struct lista *novo;
+	struct lista *p = list,*aux =list,*inicio=list,*final=list,*teste;
+	novo = (struct lista *)malloc(sizeof(struct lista));
+	
+	novo->valor = val;
+	int j=0;
+	int i=0;
+	
+	
+
+	
+	if (list == NULL){ // se a lista for vazia
+		//armazena o primeiro registro se a lista for vazia
+		novo->aa = NULL;
+		novo->pp = NULL;
+		list = novo;
+		
+	}else{
+		while(p != NULL){//
+			
+			if(p->valor <= val){//valor da lista serem menor que o VAl passsado pelo usuário
+				aux = p; // ultimo registro vai passar para o p
+				j++;	// quantas vezes ele entrou na verificacao
+			}
+			
+			p = p->pp;
+			i++; // se o j entrou menos que o i o número esta entre os dois valores, proximo, anterior, e atual
+		}
+		if(j == 0){ // primeiro da lista e menor do que todos, pois o j nao incrementou 
+
+			novo->aa = NULL;
+			list->aa = novo;
+			novo->pp = list;
+			list = novo;
+			inicio = list;
+			teste = inicio;
+			
+			
+		}else if(j<i){//valor no meio da lista, pois o i só incrimenta na prox, maior que o anterior, menor que o proximo
+			aux->pp->aa = novo;
+			novo->pp = aux->pp;
+			novo->aa = aux;
+			aux->pp = novo;
+		
+			
+		}else if (i==j){// insere no final da lista
+			aux->pp = novo;
+			novo->aa = aux;
+			//inicio->aa = aux->pp;
+				
+		}
+	
+	}
+	
+	
+	return list;
+}
+
+void remover(struct lista **list, int val){
+	struct lista *aux2,*aux1;
+	int encontrado = 0;
+	aux1 = *list; 
+	
+	if(*list == NULL)	{
+		printf("\nLista Vazia\n");
+		system("pause");
+		main();
+	}
+	else{
+		
+		printf("\nValor a Ser Removido: ");
+		listar(finalera);
+		scanf("%i",&val);
+		aux2 = buscar(*list, val);
+		
+		if(verificador==1){
+			free(aux2);
+			printf("\nElemento removido com Sucesso!\n");
+			verificador--;
+		}else{
+			
+	
+		
+		if(aux2 != NULL) 	{
+			if(aux2->pp != NULL) 	{
+				aux2->pp->aa = aux2->aa; 
+			}	if(aux2->aa != NULL) {
+				aux2->aa->pp = aux2->pp; 
+			}
+			else	{
+				*list = aux2->pp;
+			}
+			free(aux2);
+			encontrado = 1;	
+		}
+		
+		}
+		
+		if(encontrado == 1){
+			printf("\nElemento removido com Sucesso!\n");
+			verificador--;
+		}else	{
+			printf("\nElemento nao Encontrado\n");
+		}
+	}
+
+	system("pause")	;
+	main();
+}
+
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	system("cls");
+	int val;
+	int op=0;
+
+	
+	struct lista *list = inicia();
+	struct lista *final;
+
+	printf("Lista duplamente encadeada ordenada");
+	printf("\n1 - Inserir\n2 - Remover\n3 - Listar\n4 - Playlist \n5 - Sair\n\n");
+	scanf("%i",&op);
+
+	
+		if(op==1){
+			while(1<2){
+			printf("Digite um valor para ser inserido na lista(0 para cancelar):");
+			scanf("%i",&val);
+			if(val==0){
+				break;
+			}
+			if(exec==0){
+				list = insereLista(list,val);
+				finalera =list;	
+				verificador++;
+			}else if(exec>0){
+				finalera = insereLista(finalera,val);
+				verificador++;
+				
+			}
+			
+				
+			}
+			exec++;
+			system("pause");
+			main();		
+		
+		}
+		
+		if(op==2){
+			remover(&finalera,val);
+		}
+		
+		if(op==3){
+			listar(finalera);
+			system("pause");
+			main();
+		}
+		
+		
+		if(op==4){
+			printf("Digite um valor a ser buscado:");
+			scanf("%i",&val);
+			buscar(finalera, val);
+			system("pause");
+			main();
+		}
+		
+		
+		if (op==5){
+			printf("Finalizando...\n");
+			system("pause");
+		}
+
+}
+
+
