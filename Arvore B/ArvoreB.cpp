1d41ba8adac171cb931fdda9fe511e7e652c4442
FXXDEV <fxxdev@outlook.com>
2018-05-10 22:47:43 -0300

    Insert ArvoreB

--- Arvore B/ArvoreB.cpp
+++ Arvore B/ArvoreB.cpp
@@ -3,7 +3,9 @@
 #include <String.h>
 #include <locale.h>
 
-int exec=0;
+int execG=0;
+int execE=0;
+int execD=0;
 
 typedef struct ar{
 	struct ar *pp;
@@ -25,37 +27,61 @@ void inicia(b ** null){//inicializar a lista, definindo valores nulos
 
 
 int insere(b **bb,int val){
-	//bD = *bb;
-	//raiz=*bb;
-	//bE=*bb;
+	b * bD;
+	b * raiz;
+	b * bE;
 	
-	
-	if(exec==0){//primeira execução e definição de RAIZ
+	if(execG==0){//primeira execução e definição de RAIZ
 		ar * raiz = (ar *)malloc(sizeof(ar));
 		raiz->valor = val;
 		raiz->pp = bD;
 		raiz->aa = bE;
 		printf("Raiz definida com sucesso!\n");
-		exec++;
-	}else if(exec>0){
+		execG++;
+	}else if(execG>0){
 		if(val< 50){//valores a serem inseridos a esquerda
-			ar * bE = (ar *)malloc(sizeof(ar));
-			bE->valor = val;
-			bE->pp = (*bb)->pp;
-			bE->aa = *bb;
-			(*bb)->pp = bE;
-			bE->pp->aa = bE;
-			*bb = bE;
-			printf("\nValor %i inserido com sucesso a esquerda da raiz!\n\n",val);
-		}else if(val>50){//valores a serem inseridos a esquerda
-			ar * bD = (ar *)malloc(sizeof(ar));
-			bD->valor = val;
-			bD->pp = (*bb)->pp;
-			bD->aa = *bb;
-			(*bb)->pp = bD;
-			bD->pp->aa = bD;
-			*bb = bD;
-			printf("\nValor %i inserido com sucesso a direita da raiz!\n\n",val);
+			if(execE==0){
+				ar *bE = (ar *)malloc(sizeof(ar));
+				bE->valor = val;
+				bE->aa = bE;
+				bE->pp= bE;	
+				*bb = bE;
+				printf("\nValor %i inserido com sucesso a direita da raiz!\n\n",val);
+				system("pause");	
+				execE++;
+			}else{
+				ar * bE = (ar *)malloc(sizeof(ar));
+				bE->valor = val;
+				bE->pp = (*bb)->pp;
+				bE->aa = *bb;
+				(*bb)->pp = bE;
+				bE->pp->aa = bE;
+				*bb = bE;
+				printf("\nValor %i inserido com sucesso a esquerda da raiz!\n\n",val);
+			}
+			
+			
+		}else if(val>50){//valores a serem inseridos a esquerda	
+			if(execD==0){
+				ar *bD = (ar *)malloc(sizeof(ar));
+				bD->valor = val;
+				bD->aa = bD;
+				bD->pp= bD;	
+				*bb = bD;
+				printf("\nValor %i inserido com sucesso a direita da raiz!\n\n",val);
+				system("pause");	
+				execD++;
+			}else{
+				ar * bD = (ar *)malloc(sizeof(ar));
+				bD->valor = val;
+				bD->pp = (*bb)->pp;
+				bD->aa = *bb;
+				(*bb)->pp = bD;
+				bD->pp->aa = bD;
+				*bb = bD;
+				printf("\nValor %i inserido com sucesso a direita da raiz!\n\n",val);
+			}
+			
 			
 		}
 	}
@@ -96,7 +122,7 @@ int main(){
 	for(;;){
 		printf("\n1 - Inserir\n2 - Remover\n3 - Exibir\n4 - Anterior\n5 - Próximo\n6 - Cima\n7 - Sair\n\nOpção:");
 		scanf("%i",&op);
-		if(exec==0){
+		if(execG==0){
 			printf("Árvore vazia!\n");
 		}else{
 			printf("Valor atual:%i\n",val);
