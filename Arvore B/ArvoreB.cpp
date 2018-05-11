6d5383b846b540ecbe2d9f734230cb12f5dd3867
FXXDEV <fxxdev@outlook.com>
2018-05-10 22:26:42 -0300

    Update

--- Arvore B/ArvoreB.cpp
+++ Arvore B/ArvoreB.cpp
@@ -12,17 +12,56 @@ typedef struct ar{
 	int valor;
 }b;
 
+/*b *bD;// direita da raiz
+b *bE;// esquerda da raiz
+b *raiz; //raiz*/
 
 
-void inicia(b ** bb){//inicializar a lista, definindo valores nulos
-	*bb= NULL;
+void inicia(b ** null){//inicializar a lista, definindo valores nulos
+	*null= NULL;
 }
 
+
+
+
 int insere(b **bb,int val){
-	printf("Digite o valor a ser inserido");
-	scanf("%i",val);
+	//bD = *bb;
+	//raiz=*bb;
+	//bE=*bb;
 	
 	
+	if(exec==0){//primeira execução e definição de RAIZ
+		ar * raiz = (ar *)malloc(sizeof(ar));
+		raiz->valor = val;
+		raiz->pp = bD;
+		raiz->aa = bE;
+		printf("Raiz definida com sucesso!\n");
+		exec++;
+	}else if(exec>0){
+		if(val< 50){//valores a serem inseridos a esquerda
+			ar * bE = (ar *)malloc(sizeof(ar));
+			bE->valor = val;
+			bE->pp = (*bb)->pp;
+			bE->aa = *bb;
+			(*bb)->pp = bE;
+			bE->pp->aa = bE;
+			*bb = bE;
+			printf("\nValor %i inserido com sucesso a esquerda da raiz!\n\n",val);
+		}else if(val>50){//valores a serem inseridos a esquerda
+			ar * bD = (ar *)malloc(sizeof(ar));
+			bD->valor = val;
+			bD->pp = (*bb)->pp;
+			bD->aa = *bb;
+			(*bb)->pp = bD;
+			bD->pp->aa = bD;
+			*bb = bD;
+			printf("\nValor %i inserido com sucesso a direita da raiz!\n\n",val);
+			
+		}
+	}
+	
+	system("pause");
+	system("cls");
 	
 }
 
@@ -32,7 +71,9 @@ void remover(){
 
 
 void listar(){
-	
+	printf("\nError 404, página não encontrada!\n");
+	system("pause");
+	system("cls");
 }
 
 void impressaoTXT(){
@@ -40,25 +81,31 @@ void impressaoTXT(){
 }
 
 int main(){
-	setlocale(LC_ALL,"Portuguese");
 	
-	b *raiz;
+	setlocale(LC_ALL,"Portuguese");
 	b *bb;
+
 	int op;
 	int val;
 	inicia(&bb);
+	/*inicia(&bD);
+	inicia(&bE);
+	inicia(&raiz);*/
 	printf("Estrutura de Dados - Árvore B\n");
-	
+
 	for(;;){
 		printf("\n1 - Inserir\n2 - Remover\n3 - Exibir\n4 - Anterior\n5 - Próximo\n6 - Cima\n7 - Sair\n\nOpção:");
 		scanf("%i",&op);
-		printf("Valor atual:%i",val);
+		if(exec==0){
+			printf("Árvore vazia!\n");
+		}else{
+			printf("Valor atual:%i\n",val);
+		}
+		
 		if(op==1){
-			if(exec==0){
-				insere(&raiz,val);
-			}else{
-				insere(&bb,val);
-			}
+			printf("Digite o valor a ser inserido:");
+			scanf("%i",&val);
+			insere(&bb,val);	
 			
 		}
 		
